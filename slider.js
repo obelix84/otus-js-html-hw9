6f5531dcbf2ddc36cc13a3c460af49f4e9cae36d
obelix84 <65827965+obelix84@users.noreply.github.com>
2024-04-21 14:53:25 +0000

    Deploying to gh-pages from @ obelix84/otus-js-html-hw9@4d4819efe28397191b3c92830889f4f236c87c8a 🚀

---
+++ slider.js
@@ -0,0 +1,35 @@
+const slides = document.getElementById("carousel").querySelectorAll("li");
+
+function showSlide(slideNumber) {
+  const currentSlide = document.querySelector(".carousel-item__active");
+  currentSlide.removeAttribute("class");
+  currentSlide.removeAttribute("data-current");
+  currentSlide.setAttribute("class", "carousel-item");
+  slides[slideNumber].setAttribute("class", "carousel-item__active");
+  slides[slideNumber].setAttribute("data-current", "true");
+}
+
+function nextSlide() {
+  let next = 0;
+  for (let i = 0; i < slides.length; i++) {
+    if (slides[i].hasAttribute("data-current")) next = (i + 1) % slides.length;
+  }
+  showSlide(next);
+}
+
+function previousSlide() {
+  let previous = 0;
+  for (let i = 0; i < slides.length; i++) {
+    if (slides[i].hasAttribute("data-current"))
+      if (i === 0) previous = slides.length - 1;
+      else previous = i - 1;
+  }
+  showSlide(previous);
+}
+
+document
+  .querySelector(".carousel-control-right")
+  .addEventListener("click", nextSlide);
+document
+  .querySelector(".carousel-control-left")
+  .addEventListener("click", previousSlide);
